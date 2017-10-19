4429c108d8c62320a571ce7b3241d6a77645e7a9
magelix <juergen@muelln.net>
2017-10-19 14:03:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var moment = require('moment');
 var express = require('express');
 var app = express();
-
+//
 app.get('/', function (req, res) {
   var dt = moment().format();
   var name = process.env.MY_NAME || 'unknown';
